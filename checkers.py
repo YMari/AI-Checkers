cae7da9a14b30c9075d916fb9d82d5dcb5dfc8ab
owenglahn <67875686+owenglahn@users.noreply.github.com>
2020-07-05 15:34:11 -0400

    Add files via upload

---
+++ checkers.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 29 22:54:34 2020
+
+@author: Owen
+"""
+import doctest
+import pygame
+
+def move(direction):
+    """
+    (str)->bool
+    
+    Takes as input a string indicating the direction of the desired move and
+    returns true if the move was performed successfully, and false otherwise.
+    
+    """
+    return True
+    
+def winner():
+    """ 
+    ()->bool
+    
+    Returns true if there is a winner of the game, false otherwise.
+    """
+    return True
+    
+# encounters error opening pygame window
+def main():
+    pygame.init()
+    
+    size = 50
+    surface_sz = 400   # Desired physical surface size, in pixels
+
+    # Create surface of (width, height), and its window.
+    main_surface = pygame.display.set_mode((surface_sz, surface_sz))
+
+    # Set up some data to describe a small rectangle and its color
+    black, white, red = (0, 0, 0), (255, 255, 255), (255, 0, 0) 
+
+    while True:
+        
+        ev = pygame.event.poll()    # Look for any event
+        if ev.type == pygame.QUIT:  # Window close button clicked?
+            break                   #   ... leave game loop
+
+        # Update your game objects and data structures here...
+
+        # We draw everything from scratch on each frame.
+        # So first fill everything with the background color
+        main_surface.fill(white)
+        pygame.draw.rect(main_surface, red, [size, size, surface_sz, surface_sz], 1)
+
+        # Overpaint a smaller rectangle on the main surface
+        for j in range(1, 10): # x position
+            for i in range(1, 10): # y position
+                small_rect = (size*j, size*i, size, size)
+                if i % 2 == j % 2:
+                    # create a black square at every grid spot with even x and y
+                    main_surface.fill(black, small_rect) 
+
+        # display surface
+        pygame.display.flip()
+
+    pygame.quit()
+    
+    
+    
+if __name__ == "__main__":
+    #doctest.testmod()
+    main()
