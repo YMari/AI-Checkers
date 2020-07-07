e0a43159b115bfafbf73dd384e12d6e64001c2fe
owenglahn <orglahn@gmail.com>
2020-07-07 14:28:02 -0400

    Version from master with mouse clicks being developed

--- checkers.py
+++ checkers.py
@@ -1,31 +1,21 @@
 # -*- coding: utf-8 -*-
-# check branching
-"""
-Created on Mon Jun 29 22:54:34 2020
 
-@author: Owen
-"""
 import doctest
 import pygame
+import numpy
 
 # Some important variables for the entire program 
 black, white, red, blue = (0, 0, 0), (255, 255, 255), (255, 0, 0), (0, 0, 255)
 size = 50
 surface_sz = 400 # Surface size in pixels
 
+# the board holds a matrix representation of the positions of our game pieces 
+board = numpy.zeros((8,8))
+
 class Game(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
     
-    def move(xpiece, ypiece, xmove, ymove): # how do we want to choose a piece to be moved?
-        """
-        (str)->bool
-        
-        Takes as input a string indicating the direction of the desired move and
-        returns true if the move was performed successfully, and false otherwise.
-        
-        """
-        return True
         
     def winner():
         """ 
@@ -35,26 +25,43 @@ class Game(pygame.sprite.Sprite):
         """
         return True
 
-# maybe we could add a field that just is an id number (1-12) so that the player can more easily choose the piece to move
-# the number could then be displayed on the piece
-# also do we want to make a pygame.sprite.Group for each side so that we can access the pieces?
 class Piece(pygame.sprite.Sprite):
     def __init__(self, x_position, y_position, player):
         pygame.sprite.Sprite.__init__(self)
         screen = pygame.display.get_surface()
-        self.x = x_position
-        self.y = y_position
         self.player = player # for now this is a colour... but eventually we could use this for human vs computer ??
         self.radius = 20
-        pygame.draw.circle(screen, self.player, (self.x, self.y), self.radius)
- 
-# the side that the pieces belong to
-# same as colour of the piece
-# container for accessing the pieces
-class Side(pygame.sprite.Group):
-    def __init__(self, colour):
-        pygame.sprite.Group.__init__(self)
-        self.colour = colour
+        self.rect = screen.get_rect()
+        self.rect.centerx = x_position
+        self.rect.centery = y_position
+        pygame.draw.circle(screen, self.player, (self.rect.centerx, self.rect.centery), self.radius)
+
+    def move(self, x_start, y_start, x_end, y_end):
+        """
+        (int,int,int,int)->bool
+        
+        Takes as input the starting coordinates and ending coordinates of the desired move and
+        returns true if the move was performed successfully, and false otherwise.
+        
+        """
+        ## the method is updating the position of the rect but not actually moving the piece
+        # using draw.circle does work but doesn't actually move the sprite object itself
+        # not sure what to do 
+
+        self.rect.centerx = x_end
+        self.rect.centery = y_end
+
+        #screen = pygame.display.get_surface()
+        #pygame.draw.circle(screen, self.player, (self.rect.centerx, self.rect.centery), self.radius)
+        #return True
+
+class Space(pygame.sprite.Sprite):
+    def __init__(self, shape):
+        pygame.sprite.Sprite.__init__(self)
+        screen = pygame.display.get_surface()
+        self.rect = screen.get_rect()
+        screen.fill(black, shape)
+        
 
 def main():
     pygame.init()
@@ -63,44 +70,54 @@ def main():
     main_surface = pygame.display.set_mode((surface_sz, surface_sz))
     pygame.display.set_caption('Checkers')
 
+    # Fill background
+    main_surface.fill(white)
 
+    # Initialize game groups
+    pieces = pygame.sprite.RenderUpdates()
+    spaces = pygame.sprite.RenderUpdates()
+
+    # overpaint a smaller rectangle on the main surface
+    for i in range(8): # x position
+        for j in range(8): # y position
+            small_rect = (size*i, size*j, size, size)
+            if i % 2 != j % 2: # the inequality ensures that the bottom right corner is white for both players (part of the rules)
+                # create a black square at alternating gridpoints 
+                spaces.add(Space(small_rect))
+
+    # add the pieces to the board
+    for i in range(8): # x position
+        for j in range(8): # y position            
+            if i % 2 != j % 2:
+                if j < 3:
+                    pieces.add(Piece(*getPixels(i,j),blue))
+                    board[j][i] = 1 # updating the matrix with "1" for player 1's pieces
+                elif j > 4:
+                    pieces.add(Piece(*getPixels(i,j),red))
+                    board[j][i] = 2 # updating the matrix with "2" for player 2's pieces
+    
+    # display surface
+    pygame.display.flip()
+
+    piece_selected = pygame.sprite.GroupSingle()
+    space_selected = pygame.sprite.GroupSingle()
+    
     while True:
-        
-        ev = pygame.event.poll()    # Look for any event
-        if ev.type == pygame.QUIT:  # Window close button clicked?
-            break                   #   ... leave game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # if window close button clicked then leave game loop
+                break
+            if event.type == pygame.MOUSEBUTTONDOWN: # select piece to move 
+                pos = pygame.mouse.get_pos()
+                piece_selected.add(piece for piece in pieces if piece.rect.collidepoint(pos))
 
-        # Update your game objects and data structures here...
+            if event.type == pygame.MOUSEBUTTONUP: # position for piece to move
+                pos = pos_x, pos_y = pygame.mouse.get_pos()
+                space_selected.add(space for space in spaces if space.rect.collidepoint(pos))
+                orig_x, orig_y = piece_selected.sprite.rect.centerx, piece_selected.sprite.rect.centery
 
-        # Fill background
-        main_surface.fill(white)
-
-        # Initialize game groups
-        pieces = pygame.sprite.RenderUpdates()
-
-        # do we need a red border? 
-        #pygame.draw.rect(main_surface, red, [0, 0, surface_sz, surface_sz], 1)
-
-        # overpaint a smaller rectangle on the main surface
-        for i in range(8): # x position
-            for j in range(8): # y position
-                small_rect = (size*i, size*j, size, size)
-                if i % 2 != j % 2: # the inequality ensures that the bottom right corner is white for both players (part of the rules)
-                    # create a black square at alternating gridpoints 
-                    main_surface.fill(black, small_rect) 
-
-        # add the pieces to the board
-        for i in range(8): # x position
-            for j in range(8): # y position            
-                if i % 2 != j % 2:
-                    if j < 3:
-                        pieces.add(Piece(*getPixels(i,j),blue))
-                    elif j > 4:
-                        pieces.add(Piece(*getPixels(i,j),red))
-        
-        # display surface
-        pygame.display.flip()
-
+                # move is not actually moving anything !!
+                piece_selected.sprite.move(orig_x, orig_y, pos_x, pos_y)
+                pygame.display.update()
     pygame.quit()
     
 def getPixels(x_pos, y_pos):
@@ -113,7 +130,7 @@ def getPixels(x_pos, y_pos):
     """
     
     # dictionary with key as square on board and value as pixel position on screen (centre of the board square) 
-    pos_to_pixel = {0:25, 1:75, 2:125, 3:175, 4:225, 5:275, 6:325, 7:375} 
+    pos_to_pixel = {0:25, 1:75, 2:125, 3:175, 4:225, 5:275, 6:325, 7:375} # should change to soft code 
     x_pixel = pos_to_pixel[x_pos]
     y_pixel = pos_to_pixel[y_pos]
 
